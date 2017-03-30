1e4379d95ae26f300315b7b106ce09fe835fe05a
Eric <emr@setd.fr>
2017-03-30 18:47:47 +0200

    Refacto un peu plus sérieuse

--- index.js
+++ index.js
@@ -2,46 +2,58 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import sqlite3 from 'sqlite3';
 
-let file = "crountch.db";
-let urlencodedParser = bodyParser.urlencoded({ extended: false });
-let uidEncoding = 'hex';
-let db = new sqlite3.Database(file);
-let port = 3000;
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const uidEncoding = 'hex';
+const db = new sqlite3.Database("crountch.db");
+const port = 3000;
+const getRequest = "SELECT * from url WHERE ID = ?";
+const insertRequest = "INSERT INTO url VALUES (NULL, ?)";
+const regex = new RegExp("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
 
-let app = express();
+const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
 	res.render('index');
 })
 
-// TODO Check if this a real url
 app.post('/', urlencodedParser, (req, res) => {
 	let postedUrl = req.body.url;
-	db.run('INSERT INTO url VALUES (NULL, ?)', [postedUrl], function (error) {
+	if (!regex.test(postedUrl)) {
+		res.end('Check your privileges! (and your URL)');
+		return;
+	}
+
+	db.run(insertRequest, [postedUrl], function (error) {
 		if (error) throw error;
 		let encodedId = new Buffer(this.lastID.toString(), 'utf-8').toString(uidEncoding);
-		res.render('posted', { url: "http://localhost:3000/" + encodedId, lien: req.body.url });		
+		res.render('posted', { url: req.protocol + '://' + req.get('host') + "/" + encodedId, lien: req.body.url });
 	});
 })
 
-// TODO Check if id exist
 app.get('/:id', (req, res) => {
 	let decodedId = new Buffer(req.params.id, uidEncoding).toString('utf-8');
-	db.get('SELECT * from url WHERE ID = ?', [decodedId], (error, results) => {
+	db.get(getRequest, [decodedId], (error, results) => {
 		if (error) throw error;
-		res.redirect(results.URL)
+		if (!results) {
+			res.end("Unknown.");
+		} else {
+			res.redirect(results.URL);
+		}
 	});
 });
 
-// TODO Check if id exist
-app.get('/WTFIsThisShitAgain/:id', (req, res) => {
+app.get('/q/:id', (req, res) => {
 	let decodecId = new Buffer(req.params.id, uidEncoding).toString('utf-8');
-	db.get('SELECT * from url WHERE ID = ?', [decodecId], (error, results) => {
+	db.get(getRequest, [decodecId], (error, results) => {
 		if (error) throw error;
-		res.end(results.URL)
+		if (!results) {
+			res.end("Unknown.");
+		} else {
+			res.end(results.URL);
+		}
 	});
 });
 
-db.serialize(() => 	db.run("CREATE TABLE IF NOT EXISTS url (ID INTEGER PRIMARY KEY, URL TEXT NOT NULL)"));
+db.serialize(() => db.run("CREATE TABLE IF NOT EXISTS url (ID INTEGER PRIMARY KEY, URL TEXT NOT NULL)"));
 app.listen(port, () => console.log('Crountch listening on port ' + port));
