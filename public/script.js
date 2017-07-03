1e8ff8b5950188106cde166632e1abc369974ac6
Eric <emr@setd.fr>
2017-07-03 18:50:05 +0200

    Fix issues

--- public/script.js
+++ public/script.js
@@ -1,14 +1,7 @@
-var copyTextareaBtn = document.getElementById('copy-btn');
+var copyTextareaBtn = document.getElementById("copy-btn");
 
-copyTextareaBtn.addEventListener('click', function(event) {
-  var copyTextarea = document.getElementById('url');
+function clickOnCopy() {
+  var copyTextarea = document.getElementById("url");
   copyTextarea.select();
-
-  try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Copying text command was ' + msg);
-  } catch (err) {
-    console.log('Oops, unable to copy');
-  }
-});
+  document.execCommand("copy");
+}
